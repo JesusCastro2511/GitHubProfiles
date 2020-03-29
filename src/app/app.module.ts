614bf5a806876cc88dfc022d3d6382d91673ae19
Zus MexSide <jedahymc@gmail.com>
2020-03-28 20:03:11 -0600

    todo funciona perfecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,41 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { GitHubServiceService } from './services/git-hub-service.service';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { LoginComponent } from './login/login.component';
+import { SearchComponent } from './search/search.component';
+import { RegisterComponent } from './register/register.component';
+
+var firebaseConfig = {
+  apiKey: 'AIzaSyB64c3Az7iTZ6xmC7P0Brmnsz35DLFCNZI',
+    authDomain: 'meal-17c0d.firebaseapp.com',
+    databaseURL: 'https://meal-17c0d.firebaseio.com',
+    projectId: 'meal-17c0d',
+    storageBucket: 'meal-17c0d.appspot.com',
+    messagingSenderId: '798813284514',
+    appId: '1:798813284514:web:4c4b4c234ba5b420210ffb'
+}
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SearchComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule
   ],
-  providers: [],
+  providers: [GitHubServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
